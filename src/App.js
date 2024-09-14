74f2cc9c67953ed62cd80cc63455e201a8184e82
ezechavezz <ezequielmartinchavez@gmail.com>
2024-09-14 19:57:30 -0300

    14/09 19:57

--- src/App.js
+++ src/App.js
@@ -17,15 +17,15 @@ function App() {
 
 <section class='contenedor'>
   <div class='element1'>
-  <h2>About Us</h2>
+  <h3>About Us</h3>
     Somos unos estudiantes los cuales notamos la gran necesidad de crear una pagina amigable para organizar nuestras tareas, objetivos y metas.Para ello mediante nuestro curso con <span>"Generacion T"</span> hemos decidido encargarnos de esa necesidad.
   </div>
-  <div class='element3'>
+  <div class='element2'>
   <h3>Que es NoteFusion</h3>
     NoteFusion es una plataforma que permite a los usuarios gestionar sus proyectos y tareas mediante un enfoque visual y altamente personalizable. Combina la estructura de un roadmap dinámico, donde los usuarios pueden planificar y visualizar objetivos a largo plazo, con la funcionalidad de tableros al estilo Trello para organizar tareas diarias y proyectos de manera flexible.
   </div>
-  <div class='element4'>
-    <ul>
+  <div class='element3'>
+    <ul id='listaAboutUs'>
       <h3>Con NoteFusion, puedes</h3>
       <li>
         Crear y personalizar roadmaps que muestren los hitos y fases clave de tu proyecto, ajustando el flujo de trabajo según tus necesidades.</li>
@@ -40,8 +40,11 @@ function App() {
       </li>
     </ul>
   </div>
-  <div class='element5'>
+  <div class='element4'>
     insertar logo de la pag.
+  </div>
+  <div className='element5'>
+        <img src='' alt="Poner algun video a modo de gift"></img>
   </div>
 </section>
         <Popup trigger={buttonPopup} setTrigger = {setButtonPopup} > 
