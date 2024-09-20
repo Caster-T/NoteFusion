6b05a1badf86d0b7769229e1c0a86e30dcde20ab
Caster-T <tadeoperezventuro@gmail.com>
2024-09-20 08:07:36 -0300

    git me lo pide

--- src/App.js
+++ src/App.js
@@ -14,19 +14,26 @@ function App() {
           setButtonPopup(true);
           setPopupContent("Aca va el login"); // Puedes modificar este texto dinámicamente
         }}/>
+      <div>
+          <h2>About us</h2>
+          <p>
+            Somos un grupo de estudiantes que queriamos una app para 
+            poder organizarnos mejor que con las que hay disponibles
+          </p>
+          <img ></img>
+      </div>
+
 
         <Popup trigger={buttonPopup} setTrigger = {setButtonPopup} > 
           <h3>{popupContent}</h3>
           <form>
-          <label>Username: <input type="text" /></label><br/>
-          <label>Password: <input type="password" /></label><br/>
-          <button type="submit">Log in</button>
-        </form>
+            <label>Username: <input type="text" /></label><br/>
+            <label>Password: <input type="password" /></label><br/>
+            <button type="submit">Log in</button>
+          </form>
         </Popup>
     </div>
-   
   );
-
 }
 
 export default App;
